3b2fe6778a3112ce91096b84cc247625b886916c
framt <framt.freitas@hotmail.com>
2022-11-23 23:29:16 +0000

    Initial Commit

---
+++ src/main/java/com/farmmonitorbackend/FarmMonitorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.farmmonitorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmMonitorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmMonitorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farmmonitorbackend/FarmMonitorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmmonitorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmMonitorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
